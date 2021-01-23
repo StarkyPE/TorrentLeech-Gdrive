bbddaa9edac92a8bf3f8cd7c6cd05c45e1ab4b5f
StarkyPE <zenturbo123@gmail.com>
2021-01-23 19:22:14 +0800

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,15 @@
+from tobrot.sample_config import Config
+
+#Fill your all data, read readme for reference
+
+#FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
+
+class Config(Config):
+    TG_BOT_TOKEN= "1537294915:AAGNeClS8932rKoaMyWgJ_e8wmD-jBWvfsc"
+    APP_ID = 2743517
+    API_HASH = "2f595481ac3fff5b1ff9612b598d63b8"
+    OWNER_ID = 1080084254
+    AUTH_CHANNEL = [-1001435889357]
+    DESTINATION_FOLDER = "" #Name of your folder read readme(not id of the folder)
+    #Just don't fill RCLONE_CONFIG vars, insted copy your rclone.conf file in root directory
+    #if your wanted to fill -- fill your rclone config like this(Your config may have some extra value or less. so Don't worry)
